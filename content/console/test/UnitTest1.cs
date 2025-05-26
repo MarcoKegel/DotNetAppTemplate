0c9a80b5a617bdb1afa642c111837f6c82e211e9
Marco Meixner <info@meixners.org>
2025-05-26 10:47:21 +0200

    move files to 'content'

---
+++ content/console/test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Meixner.consolename_template.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
